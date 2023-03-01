c6b39427876b095ebf69a67e3f6f70d5097a0ce1
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2023-03-01 18:48:43 -0500

    inicial
    1-3-2023

---
+++ BackupAndRestorePostgres/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Delimon.Win32.IO;
+using BackupAndRestorePostgres.UtilesBackup;
+using ReneUtiles.Clases.BD;
+
+namespace BackupAndRestorePostgres
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public TemporalStorageBD TS;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.TS = new TemporalStorageBD();
+            TB_BaseDatos.Text= this.TS.getStr("bd","cinema");
+            TB_BinPostgres.Text = this.TS.getStr("binPostgres", @"C:\Program Files\PostgreSQL\13\bin\");
+            TB_Direccion.Text = this.TS.getStr("direccion", "");
+            TB_Host.Text = this.TS.getStr("host", "localhost");
+            TB_Password.Text = this.TS.getStr("password", "postgres");
+            TB_Puerto.Text = this.TS.getStr("puerto", "5432");
+            TB_Usuario.Text = this.TS.getStr("usuario", "postgres");
+        }
+
+        public void guardarEA() {
+            this.TS.put("bd", TB_BaseDatos.Text);
+            this.TS.put("binPostgres", TB_BinPostgres.Text);
+            this.TS.put("direccion", TB_Direccion.Text);
+            this.TS.put("host", TB_Host.Text);
+            this.TS.put("password", TB_Password.Text);
+            this.TS.put("puerto", TB_Puerto.Text);
+            this.TS.put("usuario", TB_Usuario.Text);
+        }
+        private void AlCerrarLaVentana(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            guardarEA();
+
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+
+        private void btnMinimizeLogin_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnCloseLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void alApretarBoton_Backup(object sender, RoutedEventArgs e)
+        {
+            FileInfo f = new FileInfo(TB_Direccion.Text);
+
+            UtilesParaBackup.BackupDatabase(
+                server: TB_Host.Text,
+             port: TB_Puerto.Text,
+             user: TB_Usuario.Text,
+             password: TB_Password.Text,
+             dbname: TB_BaseDatos.Text,
+             backupdir: UtilesParaBackup.getStrCarpetaCorrecta(f.Directory.ToString()),
+             backupFileName: f.Name,
+             backupCommandDir: UtilesParaBackup.getStrCarpetaCorrecta(TB_BinPostgres.Text)
+                );
+        }
+
+        private void alApretarBoton_Restore(object sender, RoutedEventArgs e)
+        {
+            FileInfo f = new FileInfo(TB_Direccion.Text);
+
+            UtilesParaBackup.RestoreDatabase(
+                server: TB_Host.Text,
+             port: TB_Puerto.Text,
+             user: TB_Usuario.Text,
+             password: TB_Password.Text,
+             dbname: TB_BaseDatos.Text,
+             backupdir: UtilesParaBackup.getStrCarpetaCorrecta(f.Directory.ToString()),
+             backupFileName: f.Name,
+             backupCommandDir: UtilesParaBackup.getStrCarpetaCorrecta(TB_BinPostgres.Text)
+                );
+        }
+    }
+}
---
+++ BackupAndRestorePostgres/UtilesBackup/UtilesParaBAckup.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupAndRestorePostgres.UtilesBackup
+{
+    public class UtilesParaBackup
+    {
+
+        public static string getStrCarpetaCorrecta(string carpeta) {
+            if (carpeta.EndsWith("\\")) {
+                return carpeta;
+            }
+            if (carpeta.EndsWith("/"))
+            {
+                return carpeta;
+            }
+            return carpeta + "\\";
+        }
+
+        public static string BackupDatabase(
+            string server,
+            string port,
+            string user,
+            string password,
+            string dbname,
+            string backupdir,
+            string backupFileName,
+            string backupCommandDir)
+        {
+            try
+            {
+
+                Environment.SetEnvironmentVariable("PGPASSWORD", password);
+
+                string backupFile = backupdir + backupFileName + "_" + DateTime.Now.ToString("yyyy") + "_" + DateTime.Now.ToString("MM") + "_" + DateTime.Now.ToString("dd") + ".backup";
+
+                string BackupString = " -f \"" + backupFile + "\" -F c" +
+                  " -h " + server + " -U " + user + " -p " + port + " -d " + dbname;
+
+
+                Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = backupCommandDir + "\\pg_dump.exe";
+
+                proc.StartInfo.Arguments = BackupString;
+
+                proc.StartInfo.RedirectStandardOutput = true;//for error checks BackupString
+                proc.StartInfo.RedirectStandardError = true;
+
+
+                proc.StartInfo.UseShellExecute = false;//use for not opening cmd screen
+                proc.StartInfo.CreateNoWindow = true;//use for not opening cmd screen
+
+
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+
+                return backupFile;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+        public static string RestoreDatabase(
+            string server,
+            string port,
+            string user,
+            string password,
+            string dbname,
+            string backupdir,
+            string backupFileName,
+            string backupCommandDir)
+        {
+            try
+            {
+
+                Environment.SetEnvironmentVariable("PGPASSWORD", password);
+
+                string backupFile = backupdir + backupFileName + "_" + DateTime.Now.ToString("yyyy") + "_" + DateTime.Now.ToString("MM") + "_" + DateTime.Now.ToString("dd") + ".backup";
+
+                string BackupString = " -f \"" + backupFile + "\" -F c" +
+                  " -h " + server + " -U " + user + " -p " + port + " -d " + dbname;
+
+
+                Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = backupCommandDir + "\\pg_restore.exe";
+
+                proc.StartInfo.Arguments = BackupString;
+
+                proc.StartInfo.RedirectStandardOutput = false;//for error checks BackupString
+                proc.StartInfo.RedirectStandardError = false;
+
+
+                proc.StartInfo.UseShellExecute = false;//use for not opening cmd screen
+                proc.StartInfo.CreateNoWindow = true;//use for not opening cmd screen
+
+
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+
+                return backupFile;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}

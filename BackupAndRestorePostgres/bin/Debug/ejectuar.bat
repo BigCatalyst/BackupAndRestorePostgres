9a483f5fbb066eb4e635a266ec5cdabcffd87d16
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2023-03-16 09:19:09 -0400

    ignore

--- BackupAndRestorePostgres/bin/Debug/ejectuar.bat
+++ BackupAndRestorePostgres/bin/Debug/ejectuar.bat
@@ -1,4 +1,4 @@
 @echo
 
 SET PGPASSWORD=postgres
-cd /D "C:\Program Files\PostgreSQL\13\bin\"&&dropdb --host=localhost --port=5432 --username=postgres --no-password --if-exists -f cinema&&createdb --host=localhost --port=5432 --username=postgres --no-password --owner=postgres cinema&&pg_restore --host=localhost --port=5432 --username=postgres --no-password -d cinema -v "C:\_COSAS\temporal\New folder\Nueva carpeta\save.backup"
+cd /D "C:\Program Files\PostgreSQL\13\bin\"&&dropdb --host=localhost --port=5432 --username=postgres --no-password --if-exists -f cinema&&createdb --host=localhost --port=5432 --username=postgres --no-password --owner=postgres cinema&&pg_restore --host=localhost --port=5432 --username=postgres --no-password -d cinema -v "D:\_Cosas\pincha\Paqutero Vietnamita Frances con Katanas\tercer Trabajo\ultimo save\modificado en mi pc 2.backup"

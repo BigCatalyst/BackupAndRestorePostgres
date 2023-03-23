f5ee6e8b7bf06d2b48bccd1c1f77f7acb6aa376d
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2023-03-23 08:26:50 -0400

    ejustes
    23-3-2023

--- BackupAndRestorePostgres/bin/Debug/ejectuar.bat
+++ BackupAndRestorePostgres/bin/Debug/ejectuar.bat
@@ -1,4 +1,4 @@
 @echo
 
 SET PGPASSWORD=postgres
-cd /D "C:\Program Files\PostgreSQL\13\bin\"&&dropdb --host=localhost --port=5432 --username=postgres --no-password --if-exists -f cinema&&createdb --host=localhost --port=5432 --username=postgres --no-password --owner=postgres cinema&&pg_restore --host=localhost --port=5432 --username=postgres --no-password -d cinema -v "D:\_Cosas\pincha\Paqutero Vietnamita Frances con Katanas\tercer Trabajo\ultimo save\modificado en mi pc 2.backup"
+cd /D "C:\Program Files\PostgreSQL\13\bin\"&&dropdb --host=localhost --port=5432 --username=postgres --no-password --if-exists -f cinema&&createdb --host=localhost --port=5432 --username=postgres --no-password --owner=postgres cinema&&pg_restore --host=localhost --port=5432 --username=postgres --no-password -d cinema -v "D:\_Cosas\pincha\Paqutero Vietnamita Frances con Katanas\Segundo trabajo\EsquemaBD_C#\BDPaquetero_EsquemaYUtilesConsola\BDPaquetero\bin\Debug\save_bd_2023_03_20--11-20-41.backup"
